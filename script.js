e03f73bea477b69a805b7f1c757d2c04339f97a2
Markit <138584916@users.noreply.github.com>
2024-07-20 23:35:13 +0300

    done drawing and grid size based on input

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+var color = "black"
+//default for now is black color
+
+var setSize = document.getElementById("setsize");
+setSize.addEventListener("click", genDiv);
+
 
 function genDiv(){
     //used number will be 16 divs total 4x4 for now
-    var n = 4;
+    var n = document.getElementById("size").value;
+
+    if(n < 1 || n > 16){
+        alert("Invalid entry");
+        return;
+    }
+
     var board = document.getElementById('container');
+    board.innerHTML = "";
 
     for(var i=0;i<n;i++){
         var row = document.createElement("div");
@@ -10,10 +23,19 @@ function genDiv(){
         for(var j = 0;j<n; j++){
             var cell = document.createElement("div");
             cell.className = "cell";
+            
+            cell.addEventListener("mouseover", paint)
             row.appendChild(cell);
         }
         board.appendChild(row);
     }
 }
+
+function paint(){
+    //console.log("Mouse hovered over cell");
+    this.style.backgroundColor = color;
+}
+
+//idk how i forgot that you can recall the event's object by 'this'. I feel stupid..
 
 genDiv();
