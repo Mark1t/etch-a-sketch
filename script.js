35444df32dd42650f518eb233ac6d271545a3b8c
Markit <138584916@users.noreply.github.com>
2024-07-28 01:28:03 +0300

    done grid resizing. try extra credit

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-var color = "black"
+const color = "black"
 //default for now is black color
 
-var setSize = document.getElementById("setsize");
+const setSize = document.getElementById("setsize");
 setSize.addEventListener("click", genDiv);
 
+const maxWidth = 600;
+const board = document.getElementById('container');
 
 function genDiv(){
-    //used number will be 16 divs total 4x4 for now
     var n = document.getElementById("size").value;
 
-    if(n < 1 || n > 16){
+    if(n < 1 || n > 100){
         alert("Invalid entry");
         return;
     }
-
-    var board = document.getElementById('container');
-    board.innerHTML = "";
+    
+    board.innerHTML = ""; //board clear after size change
 
     for(var i=0;i<n;i++){
         var row = document.createElement("div");
@@ -23,16 +23,20 @@ function genDiv(){
         for(var j = 0;j<n; j++){
             var cell = document.createElement("div");
             cell.className = "cell";
-            
-            cell.addEventListener("mouseover", paint)
-            row.appendChild(cell);
-        }
+            cell.addEventListener("mouseover", paint);
+            row.appendChild(cell);            
+        }        
         board.appendChild(row);
+
+        var cells = document.getElementsByClassName("cell");
+        for(let k = 0; k < cells.length; k++){
+            cells[k].style.width = maxWidth / n -2 + "px";
+            cells[k].style.height = maxWidth / n + "px";
+        }
     }
 }
 
 function paint(){
-    //console.log("Mouse hovered over cell");
     this.style.backgroundColor = color;
 }
 
